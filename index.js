608cc0ed1416e7ff649fa2f9f9897bc40dc7e18b
Joe Scott <jbs198585@gmail.com>
2022-04-26 07:22:58 -0400

    Defining our first middleware

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+
+app.use(morgan('common'))
+app.use((req, res, next) => {
+  console.log("THIS IS MY FIRST MIDDLEWARE!")
+  return next();
+  console.log("THIS IS MY FIRST MIDDLEWARE = AFTER CALLING NEXT()")
+});
+app.use((req, res, next) => {
+  console.log("THIS IS MY SECOND MIDDLEWARE!")
+  return next();
+});
+app.use((req, res, next) => {
+  console.log("THIS IS MY THIRD MIDDLEWARE!")
+  return next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('HOME PAGE!')
